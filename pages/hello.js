6c30a4febd8308a738cac836c7156d187e986c98
nascript <nascodefy@gmail.com>
2022-06-12 12:56:22 +0800

    set up project coffee

--- pages/hello.js
+++
@@ -1,7 +0,0 @@
-import styles from '../styles/Hello.module.css'
-console.log('ini style', styles)
-const Hello = () => {
-  return <div className={styles.container}>Hi I learn NEXT JS in 2022</div>
-}
-
-export default Hello
--- pages/world.js
+++
@@ -1,5 +0,0 @@
-const World = () => {
-  return <>WORLD NEXT JS in 2022</>
-}
-
-export default World
