ad58db7a5f9ff8d18615e4969fde105043e62eb7
nascript <nascodefy@gmail.com>
2022-06-12 12:50:02 +0800

    css module concep in next js

--- pages/hello.js
+++ pages/hello.js
@@ -1,5 +1,7 @@
+import styles from '../styles/Hello.module.css'
+console.log('ini style', styles)
 const Hello = () => {
-  return <>Hi I learn NEXT JS in 2022</>
+  return <div className={styles.container}>Hi I learn NEXT JS in 2022</div>
 }
 
 export default Hello
